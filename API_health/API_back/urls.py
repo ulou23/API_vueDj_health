ebf77ded4a8e017ab82db4aa547f695287432aeb
ullu <ula.lukierska@gmail.com>
2021-01-18 01:11:18 +0100

    nfz html from api.nfz

--- API_health/API_back/urls.py
+++ API_health/API_back/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import ProviderListView,DetailProvider
+from .views import ProviderListView,DetailProvider ,get_nfz
 
 urlpatterns=[
     path('',ProviderListView.as_view()),
     path('<int:pk>/',DetailProvider.as_view()),
+    path('info',get_nfz, name='get_nfz')
 ]
--- API_health/API_back/views.py
+++ API_health/API_back/views.py
@@ -10,3 +10,31 @@ class ProviderListView(generics.ListAPIView):
 class DetailProvider(generics.RetrieveAPIView):
     queryset = ProviderNHS.objects.all()
     serializer_class = ProviderSerializer
+
+
+from django.shortcuts import render
+import requests
+
+def get_nfz(request):
+    response=requests.get("https://api.nfz.gov.pl/app-itl-api/queues?format=json&case=1&province=01&benefitForChild=true&api-version=1.3")
+
+    odp = response.json()['data']
+    provider=[]
+    locality=[]
+    regons=[]
+    benefits=[]
+    covid=[]
+    for d in odp:
+        provider.append(d['attributes']['provider'])
+        locality.append(d['attributes']['locality'])
+        regons.append(d['attributes']['regon-provider'])
+        covid.append(d['attributes']['covid-19'])
+        benefits.append(d['attributes']['benefit'])
+
+    return render(request, 'API_back/nfz_info.html',{
+        'provider':provider,
+        'local':locality,
+        'regons':regons,
+        'covid':regons,
+        'benefits':benefits
+    })
